cc610b5aed36323298f9db532a62436b4ac42726
guilherme <guilhermepolicarpio@gmail.com>
2022-06-17 16:26:30 -0300

    Geração de cards e função embaralhar implementada

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let verso=['bobrossparrot','explodyparrot','fiestaparrot','metalparrot',
 'revertitparrot','tripletsparrot','unicornparrot'];
 let frente=['front'];
-let k=0;
+let k=i=j=controle=0;
+let array2=[];
 
 function iniciarJogo(){
     quantidadeCartas=prompt('Com quantas cartas você pretender jogar? Valores válidos: números pares entre 4 e 14');
@@ -16,14 +17,25 @@ function iniciarJogo(){
         iniciarJogo();
     }
 
-    let i = 0;
-    let controle=0;
+    verso.sort(comparador);
+    verso = verso.slice(0, quantidadeCartas/2);
+
+    for (let i = 0; i < verso.length; i++) {
+        array2[i] = verso[i];
+      }
+    array2.sort(comparador);
+    
+    function comparador() {
+        return Math.random() - 0.5;
+    }
+
 while(k<quantidadeCartas){
-   // for(let i=0; i < quantidadeCartas; i=i+0.5){
+
+    if(controle===1){
         let blocoJogo = document.querySelector('blocoJogo');
         blocoJogo.innerHTML +=`
         <div class="carta" onclick="flip(this)">    
-            <div class="layout-card ">
+            <div class="layout-card frente">
             <img src="Imagens/${verso[i]}.gif" alt="${verso[i]}">
             </div>
             <div class="layout-card verso">
@@ -31,22 +43,44 @@ while(k<quantidadeCartas){
             </div>
         </div>     
         `
-    k=k+1;
-    if(controle <1){
-        controle = controle+1;
+    k=k+1; i=i+1; controle=0;
     }
+
     else{
-     i=i+1;
-     controle=0;
+        let blocoJogo = document.querySelector('blocoJogo');
+        blocoJogo.innerHTML +=`
+        <div class="carta" onclick="flip(this)">    
+            <div class="layout-card frente">
+            <img src="Imagens/${array2[j]}.gif" alt="${array2[j]}">
+            </div>
+            <div class="layout-card verso">
+            <img src="Imagens/front.svg">
+            </div>
+        </div>     
+        `
+    k=k+1; j=j+1; controle= 1;
     }
+
 }}
 
 iniciarJogo();
 
 function flip(cartaEscolhida){
-   // frenteCard = cartaEscolhida.querySelector('.frente');
-   // frenteCard.classList.add('girarFrente');
+
+   statusSelecao = cartaEscolhida.querySelector('.frente');
+   let status=statusSelecao.classList.contains('girarFrente');
+
+   if(status===false){
+    frenteCard = cartaEscolhida.querySelector('.frente');
+    frenteCard.classList.add('girarFrente');
     versoCard = cartaEscolhida.querySelector('.verso');
     versoCard.classList.add('girarVerso');
+   }
 
+   else{
+    frenteCard = cartaEscolhida.querySelector('.frente');
+    frenteCard.classList.remove('girarFrente');
+    versoCard = cartaEscolhida.querySelector('.verso');
+    versoCard.classList.remove('girarVerso');
+   }
 }
